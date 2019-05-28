518d307bae80c2aabd54bddddcda9be34cd7fb70
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-05-28 21:14:29 +0530

    Create GradingStudents.py

---
+++ Subdomains/Implementation/GradingStudents.py
@@ -0,0 +1,13 @@
+def solve(grades):
+    # Complete this function
+
+n = int(input().strip())
+grades = []
+grades_i = 0
+for grades_i in range(n):
+   grades_t = int(input().strip())
+   grades.append(grades_t)
+result = solve(grades)
+print ("\n".join(map(str, result)))
+
+
